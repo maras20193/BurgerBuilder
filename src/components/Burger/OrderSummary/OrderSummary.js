179bae3919c12874a97f70a9887ad0767b1022bf
Lukasz Markowski <lucas20193@gmail.com>
2020-06-01 09:36:25 +0200

    changing Modal to class Component and adding shouldCompUpdate() method

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "./../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
-  console.log(ingredientSummary);
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log("order summary update");
+  }
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>Delicious burger with the following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <strong>Total Price: {props.price.toFixed(2)}$</strong>
-      <p>Continue to Checkaout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancel}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>Delicious burger with the following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <strong>Total Price: {this.props.price.toFixed(2)}$</strong>
+        <p>Continue to Checkaout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancel}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
 export default OrderSummary;
