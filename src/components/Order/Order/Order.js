c8891680b32c3a5c69d2b92c83f451db3f5689da
Lukasz Markowski <lucas20193@gmail.com>
2020-06-06 18:07:25 +0200

    fetched orders and display  in order route

--- src/components/Order/Order/Order.js
+++ src/components/Order/Order/Order.js
@@ -2,11 +2,35 @@ import React from "react";
 import classes from "./Orders.module.css";
 
 const Order = (props) => {
+  const ingredients = [];
+  for (let ingredientsName in props.ingredients) {
+    ingredients.push({
+      name: ingredientsName,
+      amount: props.ingredients[ingredientsName],
+    });
+  }
+  const ingredientsOutput = ingredients.map((ig) => {
+    return (
+      <span
+        key={ig.key}
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px",
+        }}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
   return (
     <div className={classes.Order}>
-      <p>Ingredients: </p>
+      <p>Ingredients: {ingredientsOutput} </p>
       <p>
-        Price: <strong>{props.price}$</strong>
+        Price: <strong>{Number.parseFloat(props.price).toFixed(2)} $</strong>
       </p>
     </div>
   );
